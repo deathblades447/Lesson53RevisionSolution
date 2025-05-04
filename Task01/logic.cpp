9b2c45d51236bd83ce1e44a1c728fa6f9b610eb2
rasslabsya <artyomlordik@gmail.com>
2025-05-04 19:31:16 +0300

    Add project files.

---
+++ Task01/logic.cpp
@@ -0,0 +1,27 @@
+﻿// Среднее арифметическое ненулевых элементов
+// [The arithmetic mean of non-zero elements]
+// 
+// Дана математическая прямоугольная матрица размером N на M.
+// Необходимо разработать функцию (или программу), которая 
+// вычисляет среднее арифметическое ненулевых элементов матрицы.
+
+#include "logic.h"
+
+double calculate_arithmetical_mean_of_nonzero_elements(int** matrix, int n, int m) {
+	if (matrix == nullptr || n <= 0 || m <= 0) {
+		return 0;
+	}
+	double sum = 0;
+	int count = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (matrix[i][j] != 0) {
+				sum += matrix[i][j];
+				count++;
+			}
+		}
+	}
+	return count > 0 ? sum / count : 0;
+}
